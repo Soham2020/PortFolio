bc1c1c33f7735fbb3a24e07bebf2acecc2cea0af
Soham Das <majumder70.ranu@gmail.com>
2021-07-06 23:55:18 +0530

    About Page added and Animate onScroll (AOS) had given

--- src/Components/Body/About/About.js
+++ src/Components/Body/About/About.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './About.css';
+import AboutPic from '../../../Assets/about.jpeg';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 export default function About () {
+    useEffect(() => {
+        AOS.init({ duration: 2000 });
+    }, [])
     return (
-        <div>
-            About page
-        </div>
+        <>
+            <div data-aos="fade-up" className="abt">
+                <div className="abt-photo">
+                        <img src={AboutPic} className="pic" alt="bio-picture" />
+                </div>
+                <div className="abt-info">
+                    <p className="para">
+                        Hello! Interestingly, I have been a student of Bio-Maths in my high school and <br/>
+                        got introduced to programming in my freshman year at <span className="clg-name">BPPIMT</span>. Since then, <br/>
+                        I started developing <span className="clg-name">Web and Android projects</span>, and currently pursuing my <br/>
+                        Bachelors in Computer Science and Engineering (2019-2023). <br /> <br />
+                        I can work efficiently in <span className="clg-name">C++</span>, <span className="clg-name">C</span>, <span className="clg-name">Reactjs</span>  
+                        {' '}and quite comformtable in Python.<br/>
+                        I have also done many projects on <span className="clg-name">MERN-Stack</span> and React-Native.
+                    </p>
+                </div>
+            </div>
+        </>
     )
 }
