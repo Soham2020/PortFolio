daae44a11f7e17f8669e37f95173e490f3c60369
Soham Das <majumder70.ranu@gmail.com>
2021-07-07 23:09:45 +0530

    Experience Page done and minor style fix

--- src/Components/Body/Work/Work.js
+++ src/Components/Body/Work/Work.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { WorkData } from '../../Data/WorkData';
 import './Work.css';
+import WorkCard from './WorkCard';
+import AOS from 'aos';
 
 export default function Work () {
+    const data = WorkData;
+    useEffect(() => {
+        AOS.init({ duration: 2000 });
+    }, []);
     return(
-        <div>
-            Work;
+        <div className="work">
+            <label className="section-title">Experience</label>
+            <div data-aos="zoom-in" className="work-list">
+                {
+                    data.map((item) => {
+                        return <WorkCard item={item} />
+                    })
+                }
+            </div>
         </div>
     )
 }
---
+++ src/Components/Data/WorkData.js
@@ -0,0 +1,18 @@
+export const WorkData = [
+    {
+        company: "Hacktober Fest 2020",
+        designation: "Open Source Contribution",
+        dateJoining: "October-2020",
+        dateEnd: "September-2020",
+        companyLogo: "https://pbs.twimg.com/profile_images/1295732034279813123/ykLQzTnJ.png",
+        work: "Hacktoberfest is a month-long celebration of open source software run by DigitalOcean in partnership with GitHub and Twilio. Hacktoberfest is open to everyone in our global community!"
+    },
+    {
+        company: "Hackerearth BPPIMT Drive",
+        designation: "Tech-Lead",
+        dateJoining: "June-2021",
+        dateEnd: "Present",
+        companyLogo: "https://i.pinimg.com/originals/6b/5c/ba/6b5cba015ccc362e4842ccacf0b381e6.png",
+        work: "The HackerEarth Campus Ambassador Program is a platform for students who are passionate about programming to start and run an active programming club in their college. A HackerEarth Campus Ambassador is a leader with a keen interest in starting and building a programming community."
+    }
+]
