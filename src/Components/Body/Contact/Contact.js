0b1fe7e698d5393de8da4d556d0e607473f012cf
Soham Das <majumder70.ranu@gmail.com>
2021-07-07 23:54:35 +0530

    Contact Page added

--- src/Components/Body/Contact/Contact.js
+++ src/Components/Body/Contact/Contact.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './Contact.css';
+import Social from '../../Common/Social/Social';
 
 export default function Contact () {
     return(
-        <div>
-            Contact;
+        <div className="contact">
+            <label className="section-title">Contact</label>
+            <div className="contact-container">
+                <div className="contact-left">
+                    <p>
+                        My inbox is always open. Whether you have a question or just want to say hello,
+                        I'll try my best to get back to you! Feel free to mail me about any relevant job updates.
+                    </p>
+                    <Social />
+                </div>
+                <div className="download">
+                    <a href="#">
+                        <i class="fi-rr-download download-icon"></i>
+                        Resume!
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
