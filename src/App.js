89e26041d3750eadabe44199f3a1ae5674b5c70a
Soham Das <majumder70.ranu@gmail.com>
2021-07-08 23:17:12 +0530

    Loader added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Home from './Components/Home/Home';
+import PropagateLoader from "react-spinners/PropagateLoader";
+
 function App() {
+  const [ loading, setLoading ] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
   return (
-    <Home />
+    <>
+      {
+        loading ?
+          <div className="loader">
+            <PropagateLoader color={"#50E3C2"} loading={loading} size={10} />
+          </div> : <Home />
+      }
+    </>
   );
 }
 
