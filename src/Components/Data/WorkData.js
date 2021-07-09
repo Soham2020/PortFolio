97a556b69f76fa9cd6751d37c2292cd46cc3a8fb
Soham Das <majumder70.ranu@gmail.com>
2021-07-09 16:47:56 +0530

    Typo fixes

--- src/Components/Data/WorkData.js
+++ src/Components/Data/WorkData.js
@@ -13,6 +13,6 @@ export const WorkData = [
         dateJoining: "June-2021",
         dateEnd: "Present",
         companyLogo: "https://i.pinimg.com/originals/6b/5c/ba/6b5cba015ccc362e4842ccacf0b381e6.png",
-        work: "The HackerEarth Campus Ambassador Program is a platform for students who are passionate about programming to start and run an active programming club in their college. A HackerEarth Campus Ambassador is a leader with a keen interest in starting and building a programming community."
+        work: "Tech-Lead."
     }
 ]
